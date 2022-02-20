dbc08bd767ca68dd3696c077b37ef802c2415625
Ognjen Koldzic <ognjen_koldzic@yahoo.de>
2022-02-20 15:59:09 +0100

    FirstStepBudgetCard

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import { Container, Stack, Button } from "react-bootstrap";
 
 function App() {
   return (
-    <Container>
+    <Container className="my-4">
       <Stack direction="horizontal" gap="2" className="mb-4">
         <h1 className="me-auto">Budgets</h1>
         <Button variant="primary">Add Budget</Button>
         <Button variant="outline-primary">Add Expense</Button>
       </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1 fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      >
+        <BudgetCard />
+      </div>
     </Container>
   );
 }
---
+++ src/components/BudgetCard.jsx
@@ -0,0 +1,11 @@
+import { Card } from "react-bootstrap";
+
+export default function BudgetCard() {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title></Card.Title>
+      </Card.Body>
+    </Card>
+  );
+}
